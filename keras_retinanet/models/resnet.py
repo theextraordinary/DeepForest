6f2849400acbfd040054fea29365f50170576643
bw4sz <benweinstein2010@gmail.com>
2020-04-27 14:40:38 -0700

    four channel init

--- keras_retinanet/models/resnet.py
+++ keras_retinanet/models/resnet.py
@@ -90,9 +90,9 @@ def resnet_retinanet(num_classes, backbone='resnet50', inputs=None, modifier=Non
     # choose default input
     if inputs is None:
         if keras.backend.image_data_format() == 'channels_first':
-            inputs = keras.layers.Input(shape=(3, None, None))
+            inputs = keras.layers.Input(shape=(4, None, None))
         else:
-            inputs = keras.layers.Input(shape=(None, None, 3))
+            inputs = keras.layers.Input(shape=(None, None, 4))
 
     # create the resnet backbone
     if backbone == 'resnet50':
--- tests/test_keras_retinanet.py
+++ tests/test_keras_retinanet.py
@@ -1,5 +1,9 @@
 #test loading of keras retinanet
 import os
+import pytest
+from keras_retinanet.preprocessing import four_channel
+from deepforest import utilities
+from deepforest import get_data
 
 def test_keras_retinanet():
     import keras_retinanet
@@ -7,3 +11,25 @@ def test_keras_retinanet():
 def test_Cython_build():
     import keras_retinanet.utils.compute_overlap  
     assert os.path.exists(keras_retinanet.utils.compute_overlap.__file__)
+    
+@pytest.fixture()
+def annotations():
+    annotations = utilities.xml_to_annotations(get_data("OSBS_029.xml"))
+    #Point at the png version for tfrecords
+    annotations.image_path = annotations.image_path.str.replace(".tif",".png")
+    
+    annotations_file = get_data("testfile_deepforest.csv")
+    annotations.to_csv(annotations_file,index=False,header=False)
+    
+    return annotations_file
+
+@pytest.fixture()
+def classes_file(annotations):
+    classes_file = utilities.create_classes(annotations)    
+    return classes_file
+    
+def test_four_channel(annotations, classes_file):
+    generator = four_channel.FourChannelGenerator(annotations, classes_file)
+    inputs, targets = generator.__getitem__(0)
+    assert inputs.shape == (1,800,800,4)
+    
